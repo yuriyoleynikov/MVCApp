c8e01eb85ee43bbc9d8519ea8536b1dc7272e7f0
yuriyoleynikov <yuriyoleynikov.ru@gmail.com>
2016-10-14 12:41:05 +0300

    Add tests

---
+++ TodoListApp/src/TodoListApp/Models/ITodoListRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoListApp.Models
+{
+    public interface ITodoListRepository
+    {
+        IEnumerable<TodoItemModel> GetTodoListByUser(string userId);
+        void DeleteItem(Guid itemId);
+        void AddItem(string userId, TodoItemModel item);
+    }
+}
