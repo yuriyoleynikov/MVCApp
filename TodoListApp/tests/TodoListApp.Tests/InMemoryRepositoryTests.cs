5045e5ab7cd0c33a8f16c8762591e1f67ea5e28a
Igor Oleinikov <igor@oleinikov.ru>
2016-10-18 09:04:19 -0700

    Fix setting up of tests.

--- TodoListApp/tests/TodoListApp.Tests/InMemoryRepositoryTests.cs
+++ TodoListApp/tests/TodoListApp.Tests/InMemoryRepositoryTests.cs
@@ -5,94 +5,112 @@ using TodoListApp.Models;
 
 namespace TodoApp.Services.Tests
 {
-    public class InMemoryRepositoryTests
-    {
-        private ITodoListRepository repository = new InMemoryTodoListRepository();
-
-        [Fact]
+	public class InMemoryRepositoryTests
+	{
+		[Fact]
 		public void GetTodoListByUser_ReturnsEmpty_WhenItIsNew()
 		{
+			ITodoListRepository repository = new InMemoryTodoListRepository();
+
 			repository.GetTodoListByUser("user").Should().BeEmpty("nothing was added yet");
 		}
 
 		[Fact]
 		public void AddItem_Fails_WhenNullAsUserIsPassed()
 		{
+			ITodoListRepository repository = new InMemoryTodoListRepository();
+
 			new Action(() => repository.AddItem(null, new TodoItem())).ShouldThrow<ArgumentNullException>().And.ParamName.Should().Be("userId");
 		}
 
 		[Fact]
 		public void AddItem_Fails_WhenNullAsItemIsPassed()
 		{
+			ITodoListRepository repository = new InMemoryTodoListRepository();
+
 			new Action(() => repository.AddItem("user", null)).ShouldThrow<ArgumentNullException>();
 		}
 
 		[Fact]
 		public void AddItem_Succeeds_WhenEverythingIsPassed()
 		{
-            repository.AddItem("user", new TodoItem { Id = Guid.NewGuid(), Name = "Item 1" });
+			ITodoListRepository repository = new InMemoryTodoListRepository();
+
+			repository.AddItem("user", new TodoItem { Id = Guid.NewGuid(), Name = "Item 1" });
 		}
 
-        [Fact]
-        public void AddItem_SucceedsTwice_WhenTwoItemsCreated()
-        {
-            repository.AddItem("user", new TodoItem { Id = Guid.NewGuid(), Name = "Item 1" });
-            repository.AddItem("user", new TodoItem { Id = Guid.NewGuid(), Name = "Item 2" });
-        }
+		[Fact]
+		public void AddItem_SucceedsTwice_WhenTwoItemsCreated()
+		{
+			ITodoListRepository repository = new InMemoryTodoListRepository();
 
-        [Fact]
-        public void GetTodoListByUser_ReturnsAddedItem_WhenAdded()
-        {
-            var item = new TodoItem { Id = Guid.NewGuid(), Name = "Item 1" };
-            repository.AddItem("user", item);
+			repository.AddItem("user", new TodoItem { Id = Guid.NewGuid(), Name = "Item 1" });
+			repository.AddItem("user", new TodoItem { Id = Guid.NewGuid(), Name = "Item 2" });
+		}
 
-            repository.GetTodoListByUser("user").Should().Equal(item);
-        }
+		[Fact]
+		public void GetTodoListByUser_ReturnsAddedItem_WhenAdded()
+		{
+			ITodoListRepository repository = new InMemoryTodoListRepository();
 
-        [Fact]
-        public void GetTodoListByUser_ReturnsAllAddedItemsInTheSameOrder()
-        {
-            var item1 = new TodoItem { Id = Guid.NewGuid(), Name = "Item 1" };
-            repository.AddItem("user", item1);
-            var item2 = new TodoItem { Id = Guid.NewGuid(), Name = "Item 2" };
-            repository.AddItem("user", item2);
-            var item3 = new TodoItem { Id = Guid.NewGuid(), Name = "Item 3" };
-            repository.AddItem("user", item3);
+			var item = new TodoItem { Id = Guid.NewGuid(), Name = "Item 1" };
+			repository.AddItem("user", item);
 
-            repository.GetTodoListByUser("user").Should().Equal(item1, item2, item3);
-        }
+			repository.GetTodoListByUser("user").Should().Equal(item);
+		}
 
-        [Fact]
-        public void GetTodoListByUser_ReturnsEmpty_WhenAddedItemsForOtherUser()
-        {
-            var item = new TodoItem { Id = Guid.NewGuid(), Name = "Item 1" };
-            repository.AddItem("user1", item);
+		[Fact]
+		public void GetTodoListByUser_ReturnsAllAddedItemsInTheSameOrder()
+		{
+			ITodoListRepository repository = new InMemoryTodoListRepository();
 
-            repository.GetTodoListByUser("user2").Should().BeEmpty();
-        }
+			var item1 = new TodoItem { Id = Guid.NewGuid(), Name = "Item 1" };
+			repository.AddItem("user", item1);
+			var item2 = new TodoItem { Id = Guid.NewGuid(), Name = "Item 2" };
+			repository.AddItem("user", item2);
+			var item3 = new TodoItem { Id = Guid.NewGuid(), Name = "Item 3" };
+			repository.AddItem("user", item3);
 
-        [Fact]
-        public void GetTodoListByUser_ReturnsEmpty_WhenSingleItemAddedAndDeleted()
-        {
-            var item = new TodoItem { Id = Guid.NewGuid(), Name = "Item 1" };
-            repository.AddItem("user", item);
-            repository.DeleteItem(item.Id);
+			repository.GetTodoListByUser("user").Should().Equal(item1, item2, item3);
+		}
 
-            repository.GetTodoListByUser("user").Should().BeEmpty();
-        }
+		[Fact]
+		public void GetTodoListByUser_ReturnsEmpty_WhenAddedItemsForOtherUser()
+		{
+			ITodoListRepository repository = new InMemoryTodoListRepository();
 
-        [Fact]
-        public void GetTodoListByUser_ReturnsWithoutDeletedItem_WhenItemAddedAndDeleted()
-        {
-            var item1 = new TodoItem { Id = Guid.NewGuid(), Name = "Item 1" };
-            repository.AddItem("user", item1);
-            var item2 = new TodoItem { Id = Guid.NewGuid(), Name = "Item 2" };
-            repository.AddItem("user", item2);
-            var item3 = new TodoItem { Id = Guid.NewGuid(), Name = "Item 3" };
-            repository.AddItem("user", item3);
-            repository.DeleteItem(item2.Id);
+			var item = new TodoItem { Id = Guid.NewGuid(), Name = "Item 1" };
+			repository.AddItem("user1", item);
 
-            repository.GetTodoListByUser("user").Should().Equal(item1, item3);
-        }
-    }
+			repository.GetTodoListByUser("user2").Should().BeEmpty();
+		}
+
+		[Fact]
+		public void GetTodoListByUser_ReturnsEmpty_WhenSingleItemAddedAndDeleted()
+		{
+			ITodoListRepository repository = new InMemoryTodoListRepository();
+
+			var item = new TodoItem { Id = Guid.NewGuid(), Name = "Item 1" };
+			repository.AddItem("user", item);
+			repository.DeleteItem(item.Id);
+
+			repository.GetTodoListByUser("user").Should().BeEmpty();
+		}
+
+		[Fact]
+		public void GetTodoListByUser_ReturnsWithoutDeletedItem_WhenItemAddedAndDeleted()
+		{
+			ITodoListRepository repository = new InMemoryTodoListRepository();
+
+			var item1 = new TodoItem { Id = Guid.NewGuid(), Name = "Item 1" };
+			repository.AddItem("user", item1);
+			var item2 = new TodoItem { Id = Guid.NewGuid(), Name = "Item 2" };
+			repository.AddItem("user", item2);
+			var item3 = new TodoItem { Id = Guid.NewGuid(), Name = "Item 3" };
+			repository.AddItem("user", item3);
+			repository.DeleteItem(item2.Id);
+
+			repository.GetTodoListByUser("user").Should().Equal(item1, item3);
+		}
+	}
 }
